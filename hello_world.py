b7a421a1fc5f063b18a39abfd72e7ee23e2845d1
Savija pasandul <Pasanduls@gmail.com>
2024-07-26 20:18:13 +0100

    hello world program - first video

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+### Running the Program
+# When you run the program, the Python interpreter reads the `helloworld.py` file and executes the `print` statement. The result is that the string `"Hello, World!"` is displayed in the console.
+
+
+### Detailed Explanation of the Code
+# `print("Hello, World!")`: print - This is a built-in Python function that outputs text to the console.
+# "Hello, World!": The parentheses `()` contain the argument to be printed. In this case, the argument is the string `"Hello, World!"`.
+# String: A string in Python is a sequence of characters enclosed in either single quotes (`'`) or double quotes (`"`). Here, `"Hello, World!"` is a string.
