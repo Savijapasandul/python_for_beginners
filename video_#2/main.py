b40d04f3d6db70010e7bd92a9207cfb484cc6435
Savija pasandul <Pasanduls@gmail.com>
2024-07-27 10:13:59 +0100

    new update to video 2

--- video_#2/main.py
+++ video_#2/main.py
@@ -1,5 +1,9 @@
 from my_class import Greeter
 
-greeter = Greeter("Alice")
+greeter = Greeter("Savija")
 
 print(greeter.greet())
+
+greeter1 = Greeter("Tommy")
+
+print(greeter1.greet())
---
+++ video_#2/variables_data_types.py
@@ -0,0 +1,33 @@
+# number = 10
+# text = "string"
+# list = [1, 2, 3, "String", "string2"]
+
+# print(number)
+
+# number = 20
+
+# print(number)
+
+# Creating and using variables
+name = "Alice"        # String variable
+age = 30              # Integer variable
+height = 5.5          # Float variable
+
+# Accessing variables
+print("Name:", name)  # Outputs: Name: Alice
+print("Age:", age)    # Outputs: Age: 30
+print("Height:", height)  # Outputs: Height: 5.5
+
+# Modifying variables
+age = 31              # Change the value of age
+print("New Age:", age)  # Outputs: New Age: 31
+
+# Changing type of variable
+age = "thirty-one"    # Now age is a string
+print("Age as text:", age)  # Outputs: Age as text: thirty-one
+
+### In this example:
+# We create and initialize variables with different types of data.
+# We access and print the values of these variables.
+# We modify the value of `age`.
+# We change the type of `age` from an integer to a string, demonstrating Python's dynamic typing.
