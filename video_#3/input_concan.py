220294f4fd365f787a31ca884ce84c1add10de94
Savija pasandul <Pasanduls@gmail.com>
2024-07-28 20:17:40 +0100

    Video 3 complete

---
+++ video_#3/input_concan.py
@@ -0,0 +1,46 @@
+### input
+# user_prompt = input(prompt)
+# name = input("Enter your name: ")
+# print("Hello, " + name + "!")
+
+### integer input
+# age  = int(input("Enter your age: "))
+# print("You are " + str(age) + " years old.")
+
+### float input
+# height = float(input("Enter your height in meters: "))
+# print("Your height is " + str(height)+ " meters.")
+
+### multiple input
+# x,y = input("Enter two numbers separated by space: ").split()
+# x = int(x)
+# y = int(y)
+# print("Sum: ", x+y)
+
+### Error handling
+
+# try:
+#     age = int(input("Enter your age: "))
+#     print("You are " + str(age) + " years old.")
+# except ValueError:
+#     print("That is not a valid age.")
+
+#_________________________________________________
+### Concatenation in Python 
+
+## + operator
+# str1 = "Hello"
+# str2 = "World!"
+# result = str1 + " " + str2
+# print(result) # outputs: Hello World!
+
+## += operator
+# str1 = "Hello"
+# str1 += " World!"
+# print(str1) # outputs: Hello World!
+
+## join()
+# words = ["Hello", "World", "from", "Python"]
+# results = " ".join(words)
+# print(results) # outputs: Hello World from Python
+
---
+++ video_#3/operators.py
@@ -0,0 +1,85 @@
+### Arithmetic Operators
+
+## Addition(+)
+# x = 5
+# y = 7 
+# print(x+y)
+
+## Subtraction(-)
+# print(x-y)
+
+## Multiplication(*)
+# print(x*y)
+
+## Division(/)
+# print(x/y)
+
+## Floor Division(//)
+# print(x//y)
+
+## Modulus(%)
+# print(x%y)
+
+## Exponentiation(**)
+# print(x**y)
+
+### Comparison Operators
+
+# x = 5
+# y = 6
+
+# Equal (==)
+# print(x == y) # false
+
+# Not equal (!=)
+# print(x!=y) # True
+
+# Greater (>)
+# print(x>y) # False
+
+# Less than (<)
+# print(x<y) # True
+
+# Greater (>=)
+# print(x>=y) # False
+
+# Less than (<=)
+# print(x<=y) # True
+
+### Logical Operators
+
+# AND (and) # Both have to be true to be True
+# print(x>2 and y<5) # False
+
+# OR (or) # Only one have to be True to be True
+# print(x>2 or y<5) # True
+
+# NOT (not)
+# print(not(x>2 or y<5)) #  = not(True) = False 
+
+### Assignment Operators
+
+# Simple assignment (=)
+# age = 20
+
+# Addition assignment (+=) 
+# age = 20 
+# age += 5
+# print(age) # outputs: 25
+
+# Substraction assignment
+# Multiplication assignment
+# Division assignment
+# Modulus assignment
+# Exponentiation assignment
+
+### Bitwise Operators
+# AND (&)
+# OR (|)
+# XOR (^)
+# NOT (~)
+
+### Membership Operators
+### Identity Operators
+
+
